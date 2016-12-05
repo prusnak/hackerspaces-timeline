15ffbef271b5ac65123a278100b8b9c07680225b
Pavol Rusnak <stick@gk2.sk>
2016-12-05 20:02:23 +0100

    run update script

--- generate-download.sh
+++ generate-download.sh
@@ -2,7 +2,9 @@
 
 # check http://hackerspaces.org/wiki/Brmlab for the current URL
 
-for i in $(seq 0 10); do
+rm download.data
+
+for i in $(seq 0 20); do
 	echo "Downloading ... $i"
 	wget --no-check-certificate "https://wiki.hackerspaces.org/Special:Ask/-5B-5BCategory:Hackerspace-5D-5D-5B-5Bhackerspace-20status::active-5D-5D/-3F%3Dhackerspace-23/-3FCountry/-3FCity/-3FLocation/-3FDate-20of-20founding/-3FWebsite/format%3Dcsv/limit%3D100/sort%3DDate_of_founding/mainlabel%3Dhackerspace/offset%3D${i}00" -O download.$i
 	tail -n +2 download.$i >> download.data
